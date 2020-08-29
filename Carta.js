f4bcaf5e44bc05376b6dd9a5f328de391b7ae4fd
AndreLuiz123 <luizandr30@yahoo.com>
2020-08-29 16:47:56 -0300

    desenha carta(método sujeito a mudanças)

--- Carta.js
+++ Carta.js
@@ -1,14 +1,18 @@
 class Carta{
 
-    constructor(numero=0, cor='red', x, y, w, h){
+    constructor(numero=0, cor='red'){
 
-
-
-
+        this.numero = numero;
+        this.cor = cor;
+        this.x = 30;
+        this.y = 30;
+        this.w = 200;
+        this.h = 300;
     }
 
-
-
-
+    desenhar(ctx){
+        ctx.fillStyle = "black";
+        ctx.fillRect(this.x,this.y,this.w, this.h);
+    }
 
 }
